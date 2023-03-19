01a57e44e847a7ebca32b3b12c1212637843506f
zc1234 <807458622@qq.com>
2023-03-19 19:23:50 +0800

    Agent Migrate
    - fix

--- src/main/kotlin/com/datapipeline/agent/entity/DpSrcNodeBasicConfig.kt
+++ src/main/kotlin/com/datapipeline/agent/entity/DpSrcNodeBasicConfig.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 data class DpSrcNodeBasicConfig(
     val `@class`: String? = null,
     val mode: String? = null,
+    @JsonIgnore val secondaryMode: SecondaryMode? = null,
     @JsonIgnore val resourceGroup: String? = null,
     @JsonIgnore val resourceGroupLabel: String? = null,
     @JsonIgnore val rollbackConfig: Map<String, Any>? = null,
